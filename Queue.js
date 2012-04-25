8551a03aec389f784da32185d90927400b7a7a60
Cheston Lee <chestonlloyd@gmail.com>
2012-04-24 22:12:52 -0700

    second bleh commit

--- Queue.js
+++ Queue.js
@@ -1,8 +1,16 @@
 var fs 			= require('fs'),
 	path		= require('path'),
 	twittter	= require('twitter-node'),
-	util		= require('util');
+	util		= require('util'),
+	yaml 		= require('yaml');
 
 var QUEUE_YAML = 'tweets.yaml';
 
-function tweet() {}
+function readYaml() {
+	fs.createReadStream(QUEUE_YAML, function() {});
+}
+
+
+function tweet() {
+
+}
